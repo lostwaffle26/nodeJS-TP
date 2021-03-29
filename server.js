d33bad32d7c06752ccf9beea59e73f8fa5d307bb
Tom RUIZ <tom.ruiz@ynov.com>
2021-03-29 17:15:02 +0200

    partie connexion mongo DB

--- server.js
+++ server.js
@@ -1,25 +1,58 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const fs = require('fs');
-const path = require('path');
+const url = 'mongodb://localhost:27017';
+const dbName = 'DBChat';
+const MongoClient = require('mongodb').MongoClient;
 
-const ServerEvent = require(path.join(__dirname, 'Controller', 'ServerEvent.js'));
+// Partie socket.io et MongoDB
+MongoClient.connect((err, instance) => {
+    if (err) {
+        process.exit(1);
+    } else {
+        console.log("Connecté à MongoDB");
 
-const socketPath = path.join(__dirname, 'Controller', 'socket.js');
-require(socketPath).listen(http, ServerEvent);
+        app.get('/', (req, res) => res.redirect('/chat'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+        const db = instance.db('DBChat');
+        const collection = db.collection('ChatsMessages');
+        collection.insertOne({
+            Pseudo: 'Steve',
+            message: 'HellO wORLD'
+        });
+     
+
+        collection.find({}, (err, rawResults) => {
+            rawResults.forEach(results => console.log(results));
+        });
+
+        http.listen(3000, () => console.log('Server READY'));
+    }
+});
 
 
-// console.log('ServerEvent', ServerEvent);
-const chatFile = fs.readFileSync('./index.html');
 
-function chatCtrl(req, res) {
-    res.end(chatFile);
-}
+/// Partie Express
 
-app.get('/', (req, res) => res.end('Hello World'));
-app.get('/chat', chatCtrl);
+// const fs = require('fs');
+// const path = require('path');
 
-app.listen(3000, () => console.log('Server READY'));
+// const ServerEvent = require(path.join(__dirname, 'Controller', 'ServerEvent.js'));
+
+// const socketPath = path.join(__dirname, 'Controller', 'socket.js');
+// require(socketPath).listen(http, ServerEvent);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// // console.log('ServerEvent', ServerEvent);
+// const chatFile = fs.readFileSync('./index.html');
+
+// function chatCtrl(req, res) {
+//     res.end(chatFile);
+// }
+
+// app.get('/', (req, res) => res.end('Hello World'));
+// app.get('/chat', chatCtrl);
+
+// app.listen(3000, () => console.log('Server READY'));
